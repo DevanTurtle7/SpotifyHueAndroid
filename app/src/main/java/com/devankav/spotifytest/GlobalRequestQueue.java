f349a34cdf956faea7b01a62148eec59ffe3f7c9
Devan Kavalchek <devankav7@gmail.com>
2021-05-18 19:25:04 -0400

    added comments

--- app/src/main/java/com/devankav/spotifytest/GlobalRequestQueue.java
+++ app/src/main/java/com/devankav/spotifytest/GlobalRequestQueue.java
@@ -1,3 +1,9 @@
+/**
+ * A request queue for making JSON requests
+ *
+ * @author Devan Kavalchek
+ */
+
 package com.devankav.spotifytest;
 
 import android.content.Context;
@@ -10,16 +16,30 @@ public class GlobalRequestQueue {
 
     private static RequestQueue queue;
 
+    /**
+     * The constructor
+     * @param context The application context
+     */
     public GlobalRequestQueue(Context context) {
+        // Only create 1 instance of the queue
         if (queue == null) {
             queue = Volley.newRequestQueue(context);
         }
     }
 
+    /**
+     * An accessor for the queue
+     * @return The request queue
+     */
     public RequestQueue getRequestQueue() {
         return queue;
     }
 
+    /**
+     * Adds a request to the queue
+     * @param request The request being added
+     * @param <T> The type of request
+     */
     public <T> void addToRequestQueue(Request<T> request) {
         queue.add(request);
     }
