ee18a3d504a4e472c69cb209a63d187061cf2457
Devan Kavalchek <devankav7@gmail.com>
2021-05-22 20:35:34 -0400

    refactored splash screen

--- app/src/main/java/com/devankav/spotifyhue/SplashScreenActivity.java
+++ app/src/main/java/com/devankav/spotifyhue/SplashScreenActivity.java
@@ -18,6 +18,29 @@ public class SplashScreenActivity extends Activity {
 
     public static final int SPLASH_SCREEN_LENGTH = 2000; // The length of the splash screen (in milliseconds)
 
+    public long getSplashScreenDuration(long startTime) {
+        long endTime = System.currentTimeMillis(); // Get the current time
+        long elapsedTime = endTime - startTime; // Get the elapsed time (how long the user has been waiting on the splash screen)
+        // Determine how much longer the user should wait on the splash screen, such that they are on the screen for a minimum of SPLASH_SCREEN_LENGTH
+        long splashScreenDuration = SPLASH_SCREEN_LENGTH - elapsedTime;
+
+        // Make sure the duration is not negative
+        if (splashScreenDuration < 0) {
+            splashScreenDuration = 0;
+        }
+
+        return splashScreenDuration;
+    }
+
+    public void exitSplashScreen(Intent intent, long splashScreenDuration) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(intent);
+            }
+        }, splashScreenDuration);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,14 +53,12 @@ public class SplashScreenActivity extends Activity {
         String recentIP = sharedPreferences.getString("recentIP", null);
         String recentUsername = sharedPreferences.getString("recentUsername", null);
 
-        boolean connected = false;
-
         if (recentIP != null && recentUsername != null) { // Check if the previous info exists
             HueConnector hueConnector = new HueConnector(this); // Create a new HueConnector instance
             BridgeState bridgeState = hueConnector.connect(""); // Attempt to connect to the bridge
             // TODO: Change to reconnect() instead of connect
 
-            BridgeStateObserver observer = new BridgeStateObserver() {
+            bridgeState.registerObserver(new BridgeStateObserver() {
                 @Override
                 public void BridgeStateUpdated(BridgeStatus bridgeStatus) {
                     Intent intent = new Intent(
@@ -45,72 +66,14 @@ public class SplashScreenActivity extends Activity {
                             bridgeStatus == BridgeStatus.CONNECTED ? MainActivity.class : ConnectActivity.class
                     );
 
-                    long endTime = System.currentTimeMillis(); // Get the current time
-                    long elapsedTime = endTime - startTime; // Get the elapsed time (how long the user has been waiting on the splash screen)
-                    // Determine how much longer the user should wait on the splash screen, such that they are on the screen for a minimum of SPLASH_SCREEN_LENGTH
-                    long splashScreenDuration = SPLASH_SCREEN_LENGTH - elapsedTime;
-
-                    // Make sure the duration is not negative
-                    if (splashScreenDuration < 0) {
-                        splashScreenDuration = 0;
-                    }
-
-                    new Handler().postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            startActivity(intent);
-                        }
-                    }, splashScreenDuration);
+                    long splashScreenDuration = getSplashScreenDuration(startTime); // Get the splash screen duration
+                    exitSplashScreen(intent, splashScreenDuration);
                 }
-            };
-
-            bridgeState.registerObserver(observer);
+            });
         } else {
             Intent intent = new Intent(SplashScreenActivity.this, ConnectActivity.class);
-            long endTime = System.currentTimeMillis(); // Get the current time
-            long elapsedTime = endTime - startTime; // Get the elapsed time (how long the user has been waiting on the splash screen)
-            // Determine how much longer the user should wait on the splash screen, such that they are on the screen for a minimum of SPLASH_SCREEN_LENGTH
-            long splashScreenDuration = SPLASH_SCREEN_LENGTH - elapsedTime;
-
-            // Make sure the duration is not negative
-            if (splashScreenDuration < 0) {
-                splashScreenDuration = 0;
-            }
-
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    startActivity(intent);
-                }
-            }, splashScreenDuration);
+            long splashScreenDuration = getSplashScreenDuration(startTime); // Get the splash screen duration
+            exitSplashScreen(intent, splashScreenDuration);
         }
-
-        /**
-         long endTime = System.currentTimeMillis(); // Get the current time
-         long elapsedTime = endTime - startTime; // Get the elapsed time (how long the user has been waiting on the splash screen)
-         // Determine how much longer the user should wait on the splash screen, such that they are on the screen for a minimum of SPLASH_SCREEN_LENGTH
-         long splashScreenDuration = SPLASH_SCREEN_LENGTH - elapsedTime;
-
-         // Make sure the duration is not negative
-         if (splashScreenDuration < 0) {
-         splashScreenDuration = 0;
-         }
-
-         // Navigate the user to the next page after the duration
-         boolean finalConnected = connected;
-         new Handler().postDelayed(new Runnable() {
-        @Override public void run() {
-        if (finalConnected) { // Check if the bridge was successfully connected to
-        // Send the user to the home screen
-        Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
-        startActivity(intent);
-        } else { // TODO: May want to add another else/if for push button state. Would probably never happen though.
-        // Send the user to the connection screen
-        Intent intent = new Intent(SplashScreenActivity.this, ConnectActivity.class);
-        startActivity(intent);
-        }
-        }
-        }, splashScreenDuration);
-         **/
     }
 }
