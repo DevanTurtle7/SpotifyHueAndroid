546b21bb996828182467f20034860dfc2558f30c
Devan Kavalchek <devankav7@gmail.com>
2021-05-30 00:12:44 -0400

    added functionality and timer to connect activity

--- app/src/main/java/com/devankav/spotifyhue/ConnectActivity.java
+++ app/src/main/java/com/devankav/spotifyhue/ConnectActivity.java
@@ -3,6 +3,7 @@ package com.devankav.spotifyhue;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.devankav.spotifyhue.bridgeConnection.BridgeState;
@@ -12,10 +13,15 @@ import com.devankav.spotifyhue.bridgeConnection.HueConnector;
 
 public class ConnectActivity extends Activity {
 
+    private static boolean running = false;
+    private static final int WAIT_TIME = 30000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connect);
+
+        running = true;
 
         Bundle bundle = getIntent().getExtras();
         String ipAddress = bundle.getString("ipAddress");
@@ -23,15 +29,63 @@ public class ConnectActivity extends Activity {
         TextView textView = findViewById(R.id.ipLabel);
         textView.setText(ipAddress);
 
+        long startTime = System.currentTimeMillis(); // Get the current time
+
         HueConnector connector = new HueConnector(this);
         BridgeStatus bridgeStatus = connector.connect(ipAddress);
         bridgeStatus.registerObserver(updated -> {
-            if (bridgeStatus.getState() != BridgeState.CONNECTED) {
-                Log.d("ConnectActivity", bridgeStatus.getState().toString());
-                connector.connect(ipAddress, bridgeStatus);
-            } else {
-                Log.d("ConnectActivity", "connected! Username: " + bridgeStatus.getUsername());
+
+            if (running) {
+                long currentTime = System.currentTimeMillis(); // Get the current time
+                long timeDiff = currentTime - startTime;
+
+                if (timeDiff < WAIT_TIME) {
+                    if (bridgeStatus.getState() != BridgeState.CONNECTED) {
+                        Log.d("ConnectActivity", bridgeStatus.getState().toString());
+                        connector.connect(ipAddress, bridgeStatus);
+                    } else {
+                        Log.d("ConnectActivity", "connected! Username: " + bridgeStatus.getUsername());
+                    }
+                } else {
+                    this.finish();
+                }
             }
         });
+
+        Thread progressUpdater = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ProgressBar progressBar = findViewById(R.id.connectionProgress);
+
+                while (running) {
+                    long currentTime = System.currentTimeMillis(); // Get the current time
+                    long timeDiff = currentTime - startTime;
+                    double percentage = timeDiff / (double) WAIT_TIME;
+                    double progress = percentage * 100;
+
+                    progressBar.setProgress((int) progress);
+
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        progressUpdater.start();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        running = false;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        running = true;
     }
 }
