c36c72561047cedd536927816f81045fc5c774c8
Devan Kavalchek <devankav7@gmail.com>
2021-06-07 22:48:00 -0400

    stubbed bridge class

--- app/src/main/java/com/devankav/spotifyhue/bridgeConnection/Bridge.java
+++ app/src/main/java/com/devankav/spotifyhue/bridgeConnection/Bridge.java
@@ -1,12 +1,30 @@
 package com.devankav.spotifyhue.bridgeConnection;
 
+import com.devankav.spotifyhue.observers.Observable;
+
 public class Bridge {
 
     private String ipAddress;
+    private String id;
     private String username;
 
-    public Bridge(String ipAddress, String username) {
+    /**
+     * The constructor
+     * @param ipAddress The IP Address of the bridge
+     * @param id The id of the bridge
+     * @param username The username used to connect to the bridge
+     */
+    public Bridge(String ipAddress, String id, String username) {
         this.ipAddress = ipAddress;
+        this.id = id;
         this.username = username;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public String getId() {
+        return id;
     }
 }
